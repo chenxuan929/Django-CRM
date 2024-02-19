d91fd58bf8d07496ea7613f1610df05ec4696317
Chenxuan Xu <xu.chenxu@northeastern.edu>
2024-02-19 18:44:27 -0500

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'Xx223322'
+
+)
+
+#a cursor object
+cursorObejct = dataBase.cursor()
+
+#a database
+cursorObejct.execute("CREATE DATABASE elderco")
+print("ALL DONE!")
