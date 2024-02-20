1f3c0897dc6c57f8e4b34dcf0ff69bc0c6e80ae2
Chenxuan Xu <xu.chenxu@northeastern.edu>
2024-02-19 21:15:30 -0500

    registration

--- mydb.py
+++ mydb.py
@@ -1,15 +1,15 @@
-import mysql.connector
+# import mysql.connector
 
-dataBase = mysql.connector.connect(
-    host = 'localhost',
-    user = 'root',
-    passwd = 'Xx223322'
+# dataBase = mysql.connector.connect(
+#     host = 'localhost',
+#     user = 'root',
+#     passwd = 'Xx223322'
 
-)
+# )
 
-#a cursor object
-cursorObejct = dataBase.cursor()
+# #a cursor object
+# cursorObejct = dataBase.cursor()
 
-#a database
-cursorObejct.execute("CREATE DATABASE elderco")
-print("ALL DONE!")
+# #a database
+# cursorObejct.execute("CREATE DATABASE elderco")
+# print("ALL DONE!")
